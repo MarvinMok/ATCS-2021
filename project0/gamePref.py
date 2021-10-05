2d790cc0b49b16c92730500b686c98ca602f8d22
marvin <marvinjmok@yahoo.com>
2021-10-05 14:08:51 -0700

    While Loops and Input Exercises

---
+++ project0/gamePref.py
@@ -0,0 +1,5 @@
+l = ["Roblox", "fortnite", "minecraft"]
+print(l)
+new_game = input("say a game: ")
+l.append(new_game)
+print(l)
---
+++ project0/manyGames.py
@@ -0,0 +1,6 @@
+l = ["Roblox", "fortnite", "minecraft"]
+print(l)
+while True:
+	new_game = input("say a game: ")
+	l.append(new_game)
+	print(l)
