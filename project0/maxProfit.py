63fd4686815f23a6bf987464c4868b20ef784924
marvin <marvinjmok@yahoo.com>
2021-10-11 21:15:28 -0700

    bugfix

--- project0/maxProfit.py
+++ project0/maxProfit.py
@@ -1,13 +1,16 @@
-def maxProfit(prices = [7,1,5,3,6,4]):
+def maxProfit(prices = [3,6,2,4]):
 	buy = prices[0]
 	sell = prices[0]
+	profit = (0, )
 	for p in prices:
 		if p < buy:
+			if sell - buy > profit[0]:
+				profit = (sell - buy, buy, sell)
 			buy = p
-			sell = p
+			sell = p		
 		if p > sell:
 			sell = p
-	return sell - buy
+	return	profit[1:]
 print(maxProfit())
 
 
